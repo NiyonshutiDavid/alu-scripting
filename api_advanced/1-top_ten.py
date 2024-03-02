6e531650d2563bf09ba78132dc509e6d1c7e10c9
NiyonshutiDavid <d.niyonshut@alustudent.com>
2024-03-02 09:09:49 +0000

    my

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python3
-"""" Top Ten Limit"""
 import requests
 
-
 def top_ten(subreddit):
-    """"top ten"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
-        .format(subreddit)
-
-    res = requests.get(url,
-                       headers={
-                           'User-Agent': 'Mozilla/5.0'})
-
-    if res.status_code != 200:
-        print(None)
+    url = "https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        if not posts:
+            print("No posts found.")
+        else:
+            for post in posts:
+                print(post['data']['title'])
     else:
-        json_response = res.json()
-        posts = json_response.get('data').get('children')
-        [print(post.get('data').get('title')) for post in posts]
+        print("None")
+
+# Example usage:
+# top_ten("programming")
+
